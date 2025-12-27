2608bcafdf576b7c58dd67d35afe70de036f532f
Veeramani-Elumalai <veeramanielumalai2004@gmail.com>
2025-12-27 16:03:48 +0530

    Backend: Added Google OAuth login with token verification and JWT integration

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -2,7 +2,9 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import { OAuth2Client } from "google-auth-library";
 
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const router = express.Router();
 
 // REGISTER
@@ -50,5 +52,48 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+//Google OAuth
+router.post("/google", async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID
+    });
+
+    const payload = ticket.getPayload();
+
+    const email = payload.email;
+    const name = payload.name;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        name,
+        email,
+        password: "google-auth-user"
+      });
+    }
+
+    const jwtToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      msg: "Google Login Success",
+      token: jwtToken,
+      user
+    });
+
+  } catch (err) {
+    res.status(400).json({ msg: "Google authentication failed" });
+  }
+});
+
 
 export default router;
